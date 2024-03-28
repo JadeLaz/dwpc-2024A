b33dc26629617d0f4d43e9e1e0e3aed6a4c7f11f
JadeLaz <160426101+JadeLaz@users.noreply.github.com>
2024-03-28 06:29:48 +0000

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = 
+{
+    entry: "./client/index.js",
+
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js",
+        publicPath: "/"
+    },
+
+    devServer: {
+        static: path.join(__dirname, "public"),
+        port: 8000,
+        host: "0.0.0.0"
+    }
+};
