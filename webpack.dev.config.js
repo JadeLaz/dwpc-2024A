df8da5d1abb65e50c5bd60172ade9e4179bdfda0
JadeLaz <160426101+JadeLaz@users.noreply.github.com>
2024-03-28 06:50:15 +0000

    configwebpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -7,9 +7,14 @@ module.exports =
     entry: "./client/index.js",
 
     output: {
-
         path: path.resolve(__dirname, "public"),
         filename: "bundle.js",
         publicPath: "/"
     },
+
+    devServer: {
+        static: path.join(__dirname, "public"),
+        port: 8000,
+        host: "0.0.0.0"
+    }
 };
